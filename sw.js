2958d9d1a6d9a008c879ab194d086a25b2ffdaf0
Dominik Deimling <dominik.deimling@basf.com>
2026-09-26 00:10:19 +0200

    update

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'ernaehrungstagebuch-pwa-v51';
+const CACHE_NAME = 'ernaehrungstagebuch-pwa-v53';
 const APP_SHELL = [
     './',
     './index.html',
-    './styles.css?v=1.1.0',
-    './app.js?v=1.1.0',
+    './styles.css?v=1.1.2',
+    './app.js?v=1.1.2',
     './src/utils.js',
     './src/state.js',
     './src/db.js',
