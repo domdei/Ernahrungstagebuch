0db610c15e75db1e4789735bde2eca84e779dd73
Dominik Deimling <dominik.deimling@basf.com>
2026-09-26 08:07:37 +0200

    css cleanup

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'ernaehrungstagebuch-pwa-v4';
+const CACHE_NAME = 'ernaehrungstagebuch-pwa-v5';
 const APP_SHELL = [
     './',
     './index.html',
-    './styles.css?v=1.1.4',
-    './app.js?v=1.1.4',
+    './styles.css?v=1.1.5',
+    './app.js?v=1.1.5',
     './src/utils.js',
     './src/state.js',
     './src/db.js',
